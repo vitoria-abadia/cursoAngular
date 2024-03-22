67e2164b647f6aee11bdc282b15c67ade89dee1d
vitorialuz229 <v.abadia@kingspanisoeste.com>
2024-03-22 09:46:27 -0300

    rotas

---
+++ src/app/alunos/alunos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos',
+  standalone: true,
+  imports: [],
+  templateUrl: './alunos.component.html',
+  styleUrl: './alunos.component.css'
+})
+export class AlunosComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CursosComponent } from './cursos/cursos.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cursos', component: CursosComponent }, 
+    { path: 'login', component: LoginComponent }, 
+    { path: '', component: HomeComponent}
+];
