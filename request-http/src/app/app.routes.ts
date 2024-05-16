1ef686a353171496f8697595da1616780e74e214
vitoria-abadia <v.abadia@kingspanisoeste.com>
2024-05-16 11:55:30 -0300

    Iniciando Modulo Http

--- request-http/src/app/app.routes.ts
+++ request-http/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        pathMatch: 'full', 
+        redirectTo: 'home'
+    }, 
+    { 
+        path: 'cursos', 
+        loadComponent: () => import('./app/cursos/cursos-lista/cursos-lista.component').then(c => c.CursosListaComponent)
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)], 
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
---
+++ request-http/src/app/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos-lista.component.html',
+  styleUrl: './cursos-lista.component.css'
+})
+export class CursosListaComponent {
+
+}
