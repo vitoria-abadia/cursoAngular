7315832c09bbd69e735e7aa7fef624779bb9f234
vitoria-abadia <v.abadia@kingspanisoeste.com>
2024-05-17 08:39:13 -0300

    Configurando variáveis de ambiente

--- request-http/src/app/app/cursos/service-cursos/curso.service.ts
+++ request-http/src/app/app/cursos/service-cursos/curso.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-
-
+import { HttpClient } from '@angular/common/http';
 import { Curso } from '../../models/cursos';
-import { pipe, tap } from 'rxjs';
-
+import { tap } from 'rxjs/operators';
+import { environment } from '../../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursoService {
 
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) { }
 
-  list() { 
-    return this.http.get<Curso[]>(this.API);
-      pipe(
-        tap(console.log)
-      );
+  list() {
+    return this.http.get<Curso[]>(this.API).pipe(
+      tap(console.log)
+    );
   }
 }
